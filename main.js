84cff361dc99fad18b144fb32870ae9981a19a2d
EzzatEldeeb <ezzateldeeb006@gmail.com>
2026-06-09 05:26:19 +0300

    patch

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// Cursor
-const cur = document.getElementById("cur"),
-  ring = document.getElementById("cur-r");
-let mx = 0,
-  my = 0,
-  rx = 0,
-  ry = 0;
-document.addEventListener("mousemove", (e) => {
-  mx = e.clientX;
-  my = e.clientY;
-});
-(function tick() {
-  cur.style.transform = `translate(${mx}px,${my}px) translate(-50%,-50%)`;
-  rx += (mx - rx) * 0.11;
-  ry += (my - ry) * 0.11;
-  ring.style.transform = `translate(${rx}px,${ry}px) translate(-50%,-50%)`;
-  requestAnimationFrame(tick);
-})();
+if (window.matchMedia("(pointer:fine)").matches) {
+  const cur = document.getElementById("cur"),
+    ring = document.getElementById("cur-r");
+  let mx = 0,
+    my = 0,
+    rx = 0,
+    ry = 0;
+  document.addEventListener("mousemove", (e) => {
+    mx = e.clientX;
+    my = e.clientY;
+  });
+  (function tick() {
+    cur.style.transform = `translate(${mx}px,${my}px) translate(-50%,-50%)`;
+    rx += (mx - rx) * 0.11;
+    ry += (my - ry) * 0.11;
+    ring.style.transform = `translate(${rx}px,${ry}px) translate(-50%,-50%)`;
+    requestAnimationFrame(tick);
+  })();
+}
 
 // Nav
 window.addEventListener("scroll", () => {
